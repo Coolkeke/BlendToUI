3e02d5efb65cdcffa6477bdaf8d2ea501dfe7e5f
1357703886 <1357703886@qq.com>
2020-12-26 17:05:31 +0800

    keke-20201226
    集成之前的视频讲样式

---
+++ Blend系列教程/BlendToUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlendToUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Data
+    {
+        public List<object> List { get; set; }
+        public Data()
+        {
+
+            List = new List<object>();
+            List.Add(new { Name = "张三", Age = 20, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "李四", Age = 10, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "王五", Age = 30, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "赵六", Age = 50, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "小明", Age = 70, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "小红", Age = 12, date = DateTime.Now, Sex = "女" });
+            List.Add(new { Name = "小绿", Age = 40, date = DateTime.Now, Sex = "男" });
+            List.Add(new { Name = "老王", Age = 33, date = DateTime.Now, Sex = "男" });
+        }
+    }
+}
